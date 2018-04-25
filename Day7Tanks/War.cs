325508d8b89dc7a37f44865f8a7c0aca21bbc19d
СауеновА <@223_8>
2018-04-25 19:02:21 +0300

    Add project files.

---
+++ Day7Tanks/War.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tanks.Models;
+
+namespace Day7Tanks
+{
+    public class War
+    {
+        List<Tank> team1;
+        List<Tank> team2;
+        public War()
+        {
+            team1 = new List<Tank>();
+            team2 = new List<Tank>();
+        }
+    }
+}
